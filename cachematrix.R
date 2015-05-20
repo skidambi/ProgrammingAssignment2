dc1c341a93581469ea03337e8e83b4e5591631c8
skidambi <skidcode@gmail.com>
2015-05-19 22:03:04 -0400

    fixed formatting

--- cachematrix.R
+++ cachematrix.R
@@ -8,26 +8,26 @@
 ##   a %*% b should return a unit matrix (build into function as next step)
 
 ## makeCacheMatrix takes an argument of type matrix and returns a list of 4 functions:
-##		set/get -> set/get the value of the matrix object inside the function object
-##		setinv/getinv ->set/get the inverse of the matrix object inside the function object
+##    set/get -> set/get the value of the matrix object inside the function object
+##    setinv/getinv ->set/get the inverse of the matrix object inside the function object
 
 makeCacheMatrix <- function(x = matrix()) {
   inv <- NULL
-	# function to set the matrix object. Reset inv to ensure recalculation
+  # function to set the matrix object. Reset inv to ensure recalculation
   set <- function(y) {
     x <<- y
     inv <<- NULL
   }
-	# function to return the stored matrix object
+  # function to return the stored matrix object
   get <- function() x
-	# function to set inv to whatever value is passed in
-	# Note: No error checking done here, calling function should do that
+  # function to set inv to whatever value is passed in
+  # Note: No error checking done here, calling function should do that
   setinv <- function(invMatrix) inv <<- invMatrix
-	# function to return the store inv object, NULL or otherwise
+  # function to return the store inv object, NULL or otherwise
   getinv <- function() inv
-	# Return a list of above 4 functions
+  # Return a list of above 4 functions
   list(set = set,
-			 get = get,
+       get = get,
        setinv = setinv,
        getinv = getinv)
 
@@ -40,7 +40,7 @@ makeCacheMatrix <- function(x = matrix()) {
 ## sets it in the function object and returns the inverse
 
 cacheSolve <- function(x, ...) {
-	## Return a matrix that is the inverse of 'x'
+  ## Return a matrix that is the inverse of 'x'
 
   if(
       class(x) != "list"
@@ -53,7 +53,7 @@ cacheSolve <- function(x, ...) {
     
   }
 
-	# see whether the "inv" object in this function object is NULL, if it isn't, return
+  # see whether the "inv" object in this function object is NULL, if it isn't, return
   # that value and exit
   invMatrix <- x$getinv()
   if(!is.null(invMatrix)) {
@@ -61,21 +61,21 @@ cacheSolve <- function(x, ...) {
     return(invMatrix)
   }
 
-	# "inv" hasn't been computed yet, so get the stored matrix and calculate its inverse
+  # "inv" hasn't been computed yet, so get the stored matrix and calculate its inverse
   origMatrix <- x$get()
-	# tryCatch to fail gracefully if solve fails
+  # tryCatch to fail gracefully if solve fails
   tryCatch(
     invMatrix <- solve(origMatrix, ...),
     error = function(e) {
-			# show a message describing what happened and exit after printing the error we received
+      # show a message describing what happened and exit after printing the error we received
       message("cacheSolve failed while solving for the matrix inverse:")
       stop(e)
     }
   )
 
-	# set "inv" in the function object to the computed inverse
-	# set the inverse
+  # set "inv" in the function object to the computed inverse
+  # set the inverse
   x$setinv(invMatrix)
-	# return the inverse explicitly in case setinv changes in future to not return it
-	invMatrix
+  # return the inverse explicitly in case setinv changes in future to not return it
+  invMatrix
 }
